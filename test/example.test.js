9713e7d6e07a5ec6d8241bba40430d11c82fa36f
its-Saab <musaababs@gmail.com>
2021-08-10 11:35:11 +0200

    Replace equal with strictEqual

--- test/example.test.js
+++ test/example.test.js
@@ -1,13 +1,13 @@
 const assert = require('assert')
 
-describe("Simple Tests", () => {
+describe('Simple Tests', () => {
   it('should return 2', () => {
-    assert.equal(1+1, 2);
-  });
+         assert.strictEqual(1 + 1, 2);
+     });
   it('should return 9', () => {
-    assert.equal(3*3, 9);
-  });
-  it('should return 2', () => {
-    assert.equal(6/6, 1);
-  });
-});
+         assert.strictEqual(3 * 3, 9);
+     });
+  it('should return 1', () => {
+         assert.strictEqual(6 / 6, 1);
+     });
+ });
